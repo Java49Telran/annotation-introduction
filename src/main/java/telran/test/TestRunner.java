76435e846e71750dc0fb5a5da68ee762a7776eae
Yuragranovsky <yuragranovsky@gmail.com>
2023-10-29 18:49:04 +0200

    HW #54 introducing annotation @BeforeEach

--- src/main/java/telran/test/TestRunner.java
+++ src/main/java/telran/test/TestRunner.java
@@ -2,7 +2,9 @@ package telran.test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
+import telran.test.annotation.BeforeEach;
 import telran.test.annotation.Test;
 
 public class TestRunner implements Runnable {
@@ -17,17 +19,40 @@ public class TestRunner implements Runnable {
 	public void run() {
 			Class<?> clazz = testObj.getClass();
 			Method[] methods = clazz.getDeclaredMethods();
-			for(Method method: methods) {
-				if(method.isAnnotationPresent(Test.class)) {
-					method.setAccessible(true);
-					try {
-						method.invoke(testObj);
-					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-						System.out.println("error: " + e.getMessage());
-					}
+			Method[] beforeEachMethods = getBeforeEachMethods(methods);
+			runTestMethods(methods, beforeEachMethods);
+
+	}
+
+	private Method[] getBeforeEachMethods(Method[] methods) {
+		
+		return Arrays.stream(methods).filter(m -> m.isAnnotationPresent(BeforeEach.class)).toArray(Method[]::new);
+	}
+
+	private void runTestMethods(Method[] methods, Method[] beforeEachMethods) {
+		for(Method method: methods) {
+			if(method.isAnnotationPresent(Test.class)) {
+				method.setAccessible(true);
+				try {
+					runMethods(beforeEachMethods);
+					method.invoke(testObj);
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					throw new RuntimeException(e);
 				}
 			}
+		}
+	}
 
+	private void runMethods(Method[] methods) {
+		for(Method method: methods) {
+				method.setAccessible(true);
+				try {
+					method.invoke(testObj);
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					throw new RuntimeException(e);
+				}
+		}
+		
 	}
 
 }
